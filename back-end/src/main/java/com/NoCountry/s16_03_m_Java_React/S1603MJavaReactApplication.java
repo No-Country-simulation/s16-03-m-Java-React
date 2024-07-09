48ff095e74e6eb3b78eba6ec2e04fe4124ede113
FacundoLB <facuu142@gmail.com>
2024-07-09 11:37:52 -0300

    iniciando back

---
+++ back-end/src/main/java/com/NoCountry/s16_03_m_Java_React/S1603MJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.NoCountry.s16_03_m_Java_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1603MJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1603MJavaReactApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/NoCountry/s16_03_m_Java_React/S1603MJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoCountry.s16_03_m_Java_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1603MJavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
