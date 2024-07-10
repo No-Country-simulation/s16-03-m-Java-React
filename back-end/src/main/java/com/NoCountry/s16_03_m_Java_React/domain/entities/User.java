5bcea4aba10924692d15a1ea689e82b369894569
FacundoLB <facuu142@gmail.com>
2024-07-10 00:15:44 -0300

    arquitectura hexagonal

---
+++ back-end/src/main/java/com/NoCountry/s16_03_m_Java_React/domain/entities/User.java
@@ -0,0 +1,23 @@
+package com.NoCountry.s16_03_m_Java_React.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.UUID)
+    private String id;
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String lastName;
+    private Long phoneNumber;
+    private String password;
+}
