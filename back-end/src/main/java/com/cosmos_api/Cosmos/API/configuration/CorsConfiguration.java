235cb4eb2f5a2d5569a87c2935da3796025fd2cc
Ziko <manuelc79@gmail.com>
2024-07-22 11:12:43 -0300

    Configurar Swagger

--- back-end/src/main/java/com/cosmos_api/Cosmos/API/configuration/CorsConfiguration.java
+++ back-end/src/main/java/com/cosmos_api/Cosmos/API/configuration/CorsConfiguration.java
@@ -1,5 +1,8 @@
 package com.cosmos_api.Cosmos.API.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,9 +15,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
