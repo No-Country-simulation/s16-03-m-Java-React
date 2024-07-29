daaa6c708f2a376b835dbbea62e90922570d27c8
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2024-07-29 15:01:48 -0400

    feat(frontend):add webs page

---
+++ frontend/modules/webs/components/heading.tsx
@@ -0,0 +1,10 @@
+const HeadingWebs = () => {
+  return (
+    <div>
+      <p className="text-xl">Mostrando todo (10)</p>
+      <div className="grid grid-cols-2"></div>
+    </div>
+  );
+};
+
+export default HeadingWebs;
